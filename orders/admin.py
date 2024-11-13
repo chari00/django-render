12da2374e06b92fbbce5ab8549b86e247b1a9366
chari00 <macharitocruzuk@gmail.com>
2024-11-13 13:23:59 +0000

    push project file

---
+++ orders/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from orders.models import Order  # Replace with your model name
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'product_name', 'quantity', 'order_date', 'completed')
+
+admin.site.register(Order, PostAdmin)
